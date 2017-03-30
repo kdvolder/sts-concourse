96444a0a90b299a1f86388bd7b6e70834a39e013
Kris De Volder <kdevolder@pivotal.io>
2017-03-30 14:44:03 -0700

    Try to upload update site zip to s3

---
+++ tasks/build-and-test-commons.sh
@@ -0,0 +1,5 @@
+xvfbrun mvn -Pe46 clean integration-test
+
+timestamp=`date +"%s"`
+file=`ls commons-git/org.springsource.ide.eclipse.commons.site/target/org.springsource.ide.eclipse.commons.site-*-SNAPSHOT.zip`
+mv ${file%.zip} output/$(basename $file)-${timestamp}.zip
