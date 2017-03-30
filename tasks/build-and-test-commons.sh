4673ef776c74ee751f651a4cc15285fe39054e69
Kris De Volder <kdevolder@pivotal.io>
2017-03-30 15:35:10 -0700

    Tests disabled (interferes with packaging??)

--- tasks/build-and-test-commons.sh
+++ tasks/build-and-test-commons.sh
@@ -5,9 +5,11 @@ workdir=`pwd`
 # So we can come back here
 cd commons-git
 mvn -Pe46 package
-#xvfb-run mvn -Pe46 integration-test
 
 cd $workdir
 timestamp=`date +"%s"`
 file=`ls commons-git/org.springsource.ide.eclipse.commons.site/target/org.springsource.ide.eclipse.commons.site-*-SNAPSHOT.zip`
 mv ${file%.zip} output/$(basename $file)-${timestamp}.zip
+
+#cd commons-git
+#xvfb-run mvn -Pe46 integration-test
