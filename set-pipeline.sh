580049785c61dae0746f8cbbcda6db1d64f31f62
Kris De Volder <kdevolder@pivotal.io>
2017-03-30 10:56:41 -0700

    Restructure repo a bit into subdirs

--- set-pipeline.sh
+++ set-pipeline.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 branch=`git rev-parse --abbrev-ref HEAD`
-fly -t tools set-pipeline --load-vars-from ${HOME}/.sts4-concourse-credentials.yml -p sts3-commons -c commons-pipeline.yml
+fly -t tools set-pipeline --load-vars-from ${HOME}/.sts4-concourse-credentials.yml -p sts3-commons -c pipelines/commons-pipeline.yml
