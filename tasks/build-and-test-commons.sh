c240bd3cff9f829f9f9290063f23327398fb1cc1
Kris De Volder <kdevolder@pivotal.io>
2017-03-30 14:56:59 -0700

    Attempt to fix build script

--- tasks/build-and-test-commons.sh
+++ tasks/build-and-test-commons.sh
@@ -1,7 +1,12 @@
 #!/bin/bash
 set -e
+workdir=`pwd` 
+
+# So we can come back here
+cd commons-git
 xvfb-run mvn -Pe46 clean integration-test
 
+cd $workdir
 timestamp=`date +"%s"`
 file=`ls commons-git/org.springsource.ide.eclipse.commons.site/target/org.springsource.ide.eclipse.commons.site-*-SNAPSHOT.zip`
 mv ${file%.zip} output/$(basename $file)-${timestamp}.zip
