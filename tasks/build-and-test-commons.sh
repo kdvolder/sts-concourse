37cdcbe33633e8fea6fa3022ba3a5640a4bcde25
Kris De Volder <kdevolder@pivotal.io>
2017-03-30 14:51:42 -0700

    Attempt to fix build script

--- tasks/build-and-test-commons.sh
+++ tasks/build-and-test-commons.sh
@@ -1,4 +1,6 @@
-xvfbrun mvn -Pe46 clean integration-test
+#!/bin/bash
+set -e
+xvfb-run mvn -Pe46 clean integration-test
 
 timestamp=`date +"%s"`
 file=`ls commons-git/org.springsource.ide.eclipse.commons.site/target/org.springsource.ide.eclipse.commons.site-*-SNAPSHOT.zip`
