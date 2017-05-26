4cfb9bbf660f1b8761e42cc2f59912fc89295655
Kris De Volder <kdevolder@pivotal.io>
2017-05-26 09:16:36 -0700

    Create snapshot of maven cache for successful test build and put on s3

--- tasks/build-and-test.sh
+++ tasks/build-and-test.sh
@@ -6,3 +6,12 @@ source cf-test-space/env.sh
 
 cd repo
 xvfb-run mvn -Pe46 -Dsts.test.failure.ignore=false integration-test
+
+# We will likely get more stable test builds if we snapshot the maven cache from
+# a successful build and reuse that for the next build. This might avoid some
+# of the issues we see around tests failing with corrupt maven repo.
+cd ${workdir}
+timestamp=`date +%s`
+tarfile=${workdir}/mvn-cache/sts3-${timestamp}.tar.gz
+tar -czvf ${tarfile} -C ${HOME} .m2/repository
+
