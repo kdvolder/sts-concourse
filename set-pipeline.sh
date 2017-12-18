301097a4ed1ca19568bbaad92649aa1709fea7fb
Kris De Volder <kdevolder@pivotal.io>
2017-12-18 15:13:04 -0800

    Tweaks

--- set-pipeline.sh
+++ set-pipeline.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
-branch=`git rev-parse --abbrev-ref HEAD`
-fly -t tools set-pipeline --load-vars-from ${HOME}/.sts4-concourse-credentials.yml -p sts3 -c pipelines/pipeline.yml
+#branch=`git rev-parse --abbrev-ref HEAD`
+branch=master
+fly -t tools set-pipeline --var "branch=${branch}" --load-vars-from ${HOME}/.sts4-concourse-credentials.yml -p sts3-${branch} -c pipelines/pipeline.yml
