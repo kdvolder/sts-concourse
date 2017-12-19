57496e920f9af7237d7884ce94ab9a60ab682c05
Kris De Volder <kdevolder@pivotal.io>
2017-12-19 11:34:02 -0800

    Grab tmp folder contents after e47 test build so we can inspect it.

--- tasks/build-and-test.sh
+++ tasks/build-and-test.sh
@@ -2,6 +2,13 @@
 set -e
 workdir=`pwd` 
 
+function finish {
+    # Grab contents of tmp dir so we can save it for later inspection
+    tarfile=${workdir}/tmp/tmp-${timestamp}.tar.gz
+    tar -czf ${tarfile} /tmp
+}
+trap finish EXIT
+
 if [ -d "mvn-cache" ]; then
     echo "Prepopulating maven cache"
     tar xzf mvn-cache/*.tar.gz -C ${HOME}
@@ -22,4 +29,6 @@ cd ${workdir}
 timestamp=`date +%s`
 tarfile=${workdir}/mvn-cache-out/sts3-${eclipse_profile}-${timestamp}.tar.gz
 tar -czf ${tarfile} -C ${HOME} .m2/repository
+
+
 
